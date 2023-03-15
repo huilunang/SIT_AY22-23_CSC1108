07fa39c2ee78b98413840132f064c07abd1d7099
kaya0910 <32829552+kaya0910@users.noreply.github.com>
2023-03-15 16:21:29 +0800

    basic maps api

--- src/app.py
+++ src/app.py
@@ -1,11 +1,12 @@
 from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
 
 # a simple page that says hello
 @app.route('/')
 def hello():
-    return 'Hello, World!'
+    return render_template('index.html')
 
 
 if __name__ == "__main__":
