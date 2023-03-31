fc15ee61547516dc1144bd8f5c31670c6f8d05eb
kaya0910 <32829552+kaya0910@users.noreply.github.com>
2023-03-31 19:16:01 +0800

    added start and end marker for html

--- src/app.py
+++ src/app.py
@@ -76,10 +76,13 @@ def generate_map():
         if data['status'] == 'OK' and data['routes']:
             route = data['routes'][0]['overview_polyline']['points']
             coords = decode_polyline(route)
+            print(coords)
 
             # plot the coordinates on a map using gmplot
             gmap = gmplot.GoogleMapPlotter.from_geocode(origin, apikey=api_key)
             gmap.plot([coord[0] for coord in coords], [coord[1] for coord in coords], 'cornflowerblue', edge_width=5)
+            gmap.marker(coords[0][0], coords[0][1], label="S", color="green")
+            gmap.marker(coords[-1][0], coords[-1][1], label="D", color="red")
 
             # create a div to hold the map and render the template with the div and user inputs
             map_html = gmap.get()
@@ -94,7 +97,7 @@ def generate_map():
                                    map_script_2=map_script_2, origin=origin, destination=destination)
 
     # render the template with the empty form
-    gmap = gmplot.GoogleMapPlotter.from_geocode('Johor Bahru, Malaysia', apikey=api_key)
+    gmap = gmplot.GoogleMapPlotter.from_geocode('Johor Bahru, Malaysia', apikey=api_key, zoom=13)
     map_html = gmap.get()
     html_finder = BeautifulSoup(map_html, 'html.parser')
     map_script = html_finder.find_all('script')
