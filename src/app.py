4141630a9694e3cc9f10e960d3218d73e9023c8f
kaya0910 <32829552+kaya0910@users.noreply.github.com>
2023-04-02 03:21:07 +0800

    lastest upates

--- src/app.py
+++ src/app.py
@@ -5,12 +5,16 @@ import gmplot
 from bs4 import BeautifulSoup
 
 from decode_polyline import decode_polyline
-from src.Classes.Route import BusRoute
+from src.Classes.Route import BusRoute, Route
 from src.Algorithms.Path import get_path, optimize_path, get_directions_of_path
 from src.Algorithms.aStarAlgo import aStar
-from src.bus_stops_init import generate_bus_stops
+from src.bus_stops_init import generate_bus_stops, get_nearest_bus_stop
+from src.maps_client import Directions
 
 app = Flask(__name__)
+
+bus_stops_dict = generate_bus_stops()
+D = Directions()
 
 
 # api_key = 'AIzaSyAYBRydi0PALfdOOPkdIjFQuiBM9uKTPTI'
@@ -26,34 +30,53 @@ def generate_map():
     api_key = "AIzaSyAYBRydi0PALfdOOPkdIjFQuiBM9uKTPTI"
 
     if request.method == "POST":
-        # origin = request.form.get("origin-input")
-        # destination = request.form.get("destination-input")
-        origin = "1.662682805, 103.5988911"
-        destination = "1.634937925, 103.6663069"
+        origin = request.form.get("origin-input")
+        destination = request.form.get("destination-input")
+        # origin = "1.662682805, 103.5988911"
+        # destination = "1.634937925, 103.6663069"
 
         # make a request to the Google Directions API
         url = f'https://maps.googleapis.com/maps/api/directions/json?origin={origin}&destination={destination}&waypoints={waypoints}&key={api_key}'
         response = requests.get(url)
         data = json.loads(response.text)
 
+        if ',' in origin:
+            origin_list = origin.split(",")
+            gmap = gmplot.GoogleMapPlotter(float(origin_list[0]), float(origin_list[1]), apikey=api_key, zoom=13)
+            gmap.marker(float(origin_list[0]), float(origin_list[1]), color="green", label="S")
+            if ',' in destination:
+                destination_list = destination.split(",")
+                gmap.marker(float(destination_list[0]), float(destination_list[1]), color="red", label="D")
+
+        else:
+            gmap = gmplot.GoogleMapPlotter.from_geocode(origin, apikey=api_key, zoom=13)
+            origin = gmplot.GoogleMapPlotter.geocode(origin, apikey=api_key)
+            destination = gmplot.GoogleMapPlotter.geocode(destination, apikey=api_key)
+            gmap.marker(float(origin[0]), float(origin[1]), color="green", label="S")
+            gmap.marker(float(destination[0]), float(destination[1]), color="red", label="D")
+
         # extract the polyline points and decode them into latitude/longitude coordinates
         if data['status'] == 'OK' and data['routes']:
 
-            origin_list = origin.split(",")
-            gmap = gmplot.GoogleMapPlotter(float(origin_list[0]), float(origin_list[1]), apikey=api_key, zoom=12)
+            start_coords = origin
+            end_coords = destination
 
-            bus_stops_dict = generate_bus_stops()
-            start = 65
-            end = 140
+            start_bus_stop = get_nearest_bus_stop(start_coords, bus_stops_dict)
+            end_bus_stop = get_nearest_bus_stop(end_coords, bus_stops_dict)
 
-            path = get_path(aStar(start, end, bus_stops_dict))
-            # printPath(path)
+            start_to_bus_stop = D.client.directions(start_coords, start_bus_stop.coords, mode='walking')
+            routes = [
+                Route("start name", start_bus_stop.name, start_coords, start_bus_stop.coords, start_to_bus_stop)]
+
+            path = get_path(aStar(start_bus_stop, end_bus_stop, bus_stops_dict))
 
             # optional optimize path below
             optimized_path = optimize_path(path)
-            routes = get_directions_of_path(optimized_path)
+            # print_optimized_path(optimized_path)
+            routes += get_directions_of_path(optimized_path)
+            bus_stop_to_end = D.client.directions(end_bus_stop.coords, end_coords, mode='walking')
+            routes.append(Route(end_bus_stop.name, "end name", end_bus_stop.coords, end_coords, bus_stop_to_end))
 
-            # route = data['routes'][0]['overview_polyline']['points']
             for r in routes:
                 route = r
                 polyline_list = route.polyline_points
@@ -62,13 +85,17 @@ def generate_map():
                 # plot the coordinates on a map using gmplot
                 if isinstance(r, BusRoute):
                     for coords in coords_list:
-                        gmap.plot([coord[0] for coord in coords], [coord[1] for coord in coords], color="blue", edge_width=5)
-                        # gmap.marker([coord[0] for coord in coords], [coord[1] for coord in coords], color="blue")
+                        gmap.plot([coord[0] for coord in coords], [coord[1] for coord in coords], color="#ff6666",
+                                  edge_width=9, edge_alpha=0.7)
+                    gmap.marker(coords_list[0][0][0], coords_list[0][0][1], color="cyan")
+                    gmap.marker(coords_list[-1][-1][0], coords_list[-1][-1][1], color="cyan")
 
                 else:
                     for coords in coords_list:
-                        gmap.plot([coord[0] for coord in coords], [coord[1] for coord in coords], color="green", edge_width=5)
-                        # gmap.marker([coord[0] for coord in coords], [coord[1] for coord in coords], color="lightblue")
+                        gmap.plot([coord[0] for coord in coords], [coord[1] for coord in coords], color="green",
+                                  edge_width=5, edge_alpha=1.0)
+                    gmap.marker(coords_list[0][0][0], coords_list[0][0][1], color="cyan")
+                    gmap.marker(coords_list[-1][-1][0], coords_list[-1][-1][1], color="cyan")
 
             # create html elements to insert into html template
             map_html = gmap.get()
@@ -87,7 +114,7 @@ def generate_map():
             for instruction in instructions_list:
                 instruction_str += f"<b>Step {step}:</b> {instruction}<br>"
                 step += 1
-            map_instructions = f'<div id="map_instruct" class="map_instruct_class"><p>{instruction_str}</p></div>'
+            map_instructions = f'<div id="map_instruct"><p>{instruction_str}</p></div>'
 
             return render_template("route.html", map_div_1=map_div_1, map_script_1=map_script_1,
                                    map_script_2=map_script_2, map_route=map_route,
